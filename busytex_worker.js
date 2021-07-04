a6c85f3351dfb7a2c8db793756f422c6f8df9d35
Vadim Kantorov <vadimkantorov@gmail.com>
2021-07-04 23:00:28 +0200

    ...

--- busytex_worker.js
+++ busytex_worker.js
@@ -6,6 +6,7 @@ onmessage = async ({data : {files, main_tex_path, bibtex, busytex_wasm, busytex_
 {
     if(busytex_wasm && busytex_js && preload_data_packages_js)
         self.pipeline = new BusytexPipeline(busytex_js, busytex_wasm, preload_data_packages_js, texmf_local, msg => postMessage({print : msg}), preload, BusytexPipeline.ScriptLoaderWorker);
+
     else if(files && self.pipeline)
         postMessage(await self.pipeline.compile(files, main_tex_path, bibtex, verbose, driver, data_packages_js))
 };
