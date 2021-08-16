75aa9e9f69abc3fc8b18efaa660389aa93e20d7c
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-16 18:40:00 +0200

    ...

--- README.md
+++ README.md
@@ -71,7 +71,7 @@ make clean
 # wasm version, download latest compiled assets, launch server.py and then go to http://localhost:8080/example/example.html
 mkdir -p dist
 wget -P dist --backups=1 $(printf "https://github.com/busytex/busytex/releases/latest/download/%s " busytex_pipeline.js busytex_worker.js    busytex.wasm busytex.js texlive-basic.js texlive-basic.data    ubuntu-texlive-latex-extra.data ubuntu-texlive-latex-extra.js    ubuntu-texlive-latex-recommended.data ubuntu-texlive-latex-recommended.js    ubuntu-texlive-science.data ubuntu-texlive-science.js)
-python3 example/serve.py
+python3 example/example.py
 
 # native version
 bash example/example.sh
---
+++ example/example.py
@@ -0,0 +1,30 @@
+import sys
+import argparse
+assert tuple(sys.version_info) >= (3, 8), 'http.server.ThreadingHTTPServer available in Python 3.8+, your Python version is: ' + sys.version
+
+import http.server
+import socketserver
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', type = int, default = 8080)
+parser.add_argument('--server', default = 'ThreadingHTTPServer', choices = ['ThreadingHTTPServer', 'TCPServer'])
+args = parser.parse_args()
+
+ext_map = {
+    '.manifest': 'text/cache-manifest',
+    '.html': 'text/html',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg':	'image/svg+xml',
+    '.css':	'text/css',
+    '.js':	'application/x-javascript',
+    '.wasm': 'application/wasm',
+    '': 'application/octet-stream'
+}
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map = ext_map
+httpd = (http.server.ThreadingHTTPServer if args.server == 'ThreadingHTTPServer' else socketserver.TCPServer)(('', args.port), Handler)
+
+print(f'serving at http://localhost:{args.port}')
+httpd.serve_forever()
