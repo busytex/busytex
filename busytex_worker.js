d14a53d7955d2b309f92a15f16b5b9e11b363c29
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-18 00:24:58 +0200

    ...

--- busytex_worker.js
+++ busytex_worker.js
@@ -2,13 +2,28 @@ importScripts('busytex_pipeline.js');
 
 self.pipeline = null;
 
-//TODO: handle exceptions and post back error log
-
 onmessage = async ({data : {files, main_tex_path, bibtex, busytex_wasm, busytex_js, preload_data_packages_js, data_packages_js, texmf_local, preload, verbose, driver}}) => 
 {
     if(busytex_wasm && busytex_js && preload_data_packages_js)
-        self.pipeline = new BusytexPipeline(busytex_js, busytex_wasm, data_packages_js, preload_data_packages_js, texmf_local, msg => postMessage({print : msg}), preload, BusytexPipeline.ScriptLoaderWorker);
-
+    {
+        try
+        {
+            self.pipeline = new BusytexPipeline(busytex_js, busytex_wasm, data_packages_js, preload_data_packages_js, texmf_local, msg => postMessage({print : msg}), preload, BusytexPipeline.ScriptLoaderWorker);
+        }
+        catch(err)
+        {
+            postMessage({exception: 'Exception during initialization: ' + err.toString()});
+        }
+    }
     else if(files && self.pipeline)
-        postMessage(await self.pipeline.compile(files, main_tex_path, bibtex, verbose, driver, data_packages_js))
+    {
+        try
+        {
+            postMessage(await self.pipeline.compile(files, main_tex_path, bibtex, verbose, driver, data_packages_js))
+        }
+        catch(err)
+        {
+            postMessage({exception: 'Exception during compilation: ' + err.toString()});
+        }
+    }
 };
