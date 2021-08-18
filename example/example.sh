20b67b0be865cc8ac6267c49961a95d88b9dd8dd
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-18 19:08:11 +0200

    ...

--- example/example.sh
+++ example/example.sh
@@ -4,9 +4,7 @@ export DIST=$PWD/dist-native
 export XETEXFMT=$DIST/xelatex.fmt
 export PDFTEXFMT=$DIST/pdflatex.fmt
 export LUATEXFMT=$DIST/lualatex.fmt
-export BUSYXETEX=$DIST/busytex
-export BUSYPDFTEX=$DIST/busytex
-export BUSYLUATEX=$DIST/busytex
+export BUSYTEX=$DIST/busytex
 
 export TEXMFDIST=$DIST/texlive/texmf-dist
 export TEXMFVAR=$DIST/texlive/texmf-dist/texmf-var
@@ -15,17 +13,21 @@ export FONTCONFIG_PATH=$DIST
 
 cd example
 
-$BUSYXETEX xetex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XETEXFMT example.tex
-$BUSYXETEX bibtex8 --8bit example.aux
-$BUSYXETEX xetex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XETEXFMT example.tex
-$BUSYXETEX xetex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XETEXFMT example.tex
-$BUSYXETEX xdvipdfmx -o example_xetex.pdf example.xdv
+$BUSYTEX
+
+$BUSYTEX xetex --version
+$BUSYTEX xetex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XETEXFMT example.tex
+$BUSYTEX bibtex8 --8bit example.aux
+$BUSYTEX xetex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XETEXFMT example.tex
+$BUSYTEX xetex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XETEXFMT example.tex
+$BUSYTEX xdvipdfmx -o example_xetex.pdf example.xdv
 rm example.aux
 
-$BUSYPDFTEX pdftex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --fmt $PDFTEXFMT example.tex
-$BUSYPDFTEX bibtex8 --8bit example.aux
-$BUSYPDFTEX pdftex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --fmt $PDFTEXFMT example.tex
-$BUSYPDFTEX pdftex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --fmt $PDFTEXFMT example.tex
+$BUSYTEX pdftex --version
+$BUSYTEX pdftex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --fmt $PDFTEXFMT example.tex
+$BUSYTEX bibtex8 --8bit example.aux
+$BUSYTEX pdftex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --fmt $PDFTEXFMT example.tex
+$BUSYTEX pdftex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --fmt $PDFTEXFMT example.tex
 mv example.pdf example_pdftex.pdf
 rm example.aux
 
