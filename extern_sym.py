43f17476ed3b9c39c934921b8b52aafca6b70c68
Vadim Kantorov <vadimkantorov@gmail.com>
2021-09-01 22:57:41 +0200

    ...

--- extern_sym.py
+++ extern_sym.py
@@ -4,8 +4,11 @@ lines = []
 
 with open(sys.argv[1]) as f:
     for l in f:
-        if l.startswith('EXTERN') and any(' ' + sym in l for sym in sys.argv[2:]):
+        symok = any(' ' + sym in l for sym in sys.argv[2:])
+        if l.startswith('EXTERN') and symok:
             lines.append(l.replace('EXTERN', 'extern'))
+        elif symok:
+            lines.append(l.replace('  ', '  extern'))
         else:
             lines.append(l)
 
