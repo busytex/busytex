66fa320b2d70c42f00a308e47f90a50a5f911abc
Vadim Kantorov <vadimkantorov@gmail.com>
2021-09-02 00:11:52 +0200

    ...

--- extern_sym.py
+++ extern_sym.py
@@ -1,11 +1,16 @@
 import sys;
 
-syms = list(filter(bool, sys.argv[2:]))
+syms = set(filter(bool, sys.argv[2:]))
 
 lines = []
 with open(sys.argv[1]) as f:
     for l in f:
-        symok = any((' ' + sym) in l for sym in syms)
+        for sym in syms:
+            if ' ' + sym in l:
+                print('SYM [', sym, '] in [', l, ']')
+                symok = True
+
+        symok = any(('  ' + sym) in l for sym in syms)
         if l.startswith('EXTERN') and symok:
             lines.append(l.replace('EXTERN', 'extern'))
         elif symok:
