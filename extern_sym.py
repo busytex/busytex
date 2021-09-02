a34500e5316b34f95a91a3ef904ecd0f206c548e
Vadim Kantorov <vadimkantorov@gmail.com>
2021-09-02 12:02:51 +0200

    ...

--- extern_sym.py
+++ extern_sym.py
@@ -10,10 +10,13 @@ with open(sys.argv[1]) as f:
             if (' ' + sym + ' ') in l or (' ' + sym + '[') in l:
                 print('SYM [', sym, '] in [', l, ']', file = sys.stderr)
                 symok = True
+                break
 
         if symok and l.startswith('EXTERN'):
+            print('REP1', sym, file = sys.stderr)
             lines.append(l.replace('EXTERN', 'extern'))
         elif symok:
+            print('REP2', sym, file = sys.stderr)
             lines.append(l.replace(' ' + sym, ' extern ' + sym))
         else:
             lines.append(l)
