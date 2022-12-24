641a96606da038dc9c3c58b423e9116466aeaf10
Vadim Kantorov <vadimkantorov@gmail.com>
2022-12-25 00:15:37 +0200

    ...

--- busyweb2c.c
+++ busyweb2c.c
@@ -24,8 +24,8 @@ extern int busymain_web2c(int argc, char* argv[]);
 extern int busymain_ctangle(int argc, char* argv[]);
 extern int busymain_otangle(int argc, char* argv[]);
 extern int busymain_tangle(int argc, char* argv[]);
-//extern int busymain_tangleboot(int argc, char* argv[]);
-//extern int busymain_ctangleboot(int argc, char* argv[]);
+extern int busymain_tangleboot(int argc, char* argv[]);
+extern int busymain_ctangleboot(int argc, char* argv[]);
 //extern int busymain_tie(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
@@ -41,8 +41,8 @@ int main(int argc, char* argv[])
     APPLET(ctangle, ctangle)
     APPLET(otangle, otangle)
     APPLET(tangle, tangle)
-    //APPLET(tangleboot, tangleboot)
-    //APPLET(ctangleboot, ctangleboot)
+    APPLET(tangleboot, tangleboot)
+    APPLET(ctangleboot, ctangleboot)
     //APPLET(tie, tie)
 
     return 1;
