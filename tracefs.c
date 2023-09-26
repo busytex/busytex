4d8c1b64e78eda27ae2f4e1fb12ee6439f90357a
Vadim Kantorov <vadimkantorov@gmail.com>
2023-09-26 15:49:33 +0200

    Delete tracefs.c

--- tracefs.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-FILE* orig_fopen(const char *path, const char *mode);
-FILE* fopen(const char *path, const char *mode)
-{
-    fprintf(stderr, "log_file_access_preload: fopen(\"%s\", \"%s\")\n", path, mode);
-    return orig_fopen(path, mode);
-}
