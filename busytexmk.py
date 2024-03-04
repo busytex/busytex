7b11bbbef8187c1721abca4f2c8b48d428703f3b
Vadim Kantorov <vadimkantorov@gmail.com>
2024-03-04 17:38:41 +0100

    ...

--- busytexmk.py
+++ busytexmk.py
@@ -46,14 +46,22 @@ error_messages_fatal = [
     'LaTeX Error: Missing \\begin{document}', 
     '\\end{thebibliography}', 
     'That was a fatal error',
-    'Fatal format file error'
+    'Fatal format file error',
+    
+    'Could not locate a virtual/physical font for TFM', # xdvipdfmx:warning: 
+    'There are no valid font mapping entry for this font', # xdvipdfmx:warning: 
+    'was assumed but failed to locate that font', # xdvipdfmx:warning: 
+    'Color stack underflow. Just ignore.', # xdvipdfmx:warning: 
 ]
 error_messages_extra = [
     'no output PDF file produced', 
+    'No output PDF file written',
     'No pages of output.'
 ]
 
 error_messages_all = error_messages_extra + error_messages_fatal
+
+has_error = lambda cmdres, errors: any(e.encode() in cmdres.stdout + cmdres.stderr for e in errors)
 
 SOURCE_DATE_EPOCH = 1234567890
 
@@ -93,7 +101,7 @@ def pdflatex(tex_relative_path, busytex, cwd, DIST, bibtex, log = None):
     texmflog = 'texmf.log' # /tmp/texmf.log
     missfontlog = 'missfont.log'
     fmt = os.path.join(DIST, 'pdflatex.fmt')
-    xdv_path, pdf_path, log_path, aux_path, blg_path, bbl_path = [tex_relative_path.removesuffix('.tex') + ext for ext in ['.xdv', '.pdf', '.log', '.aux', '.blg', '.bbl']] # https://github.com/github/gitignore/blob/main/TeX.gitignore
+    pdf_path, log_path, aux_path, blg_path, bbl_path = [tex_relative_path.removesuffix('.tex') + ext for ext in ['.pdf', '.log', '.aux', '.blg', '.bbl']] # https://github.com/github/gitignore/blob/main/TeX.gitignore
 
     env = dict(
         TEXMFDIST         = os.path.join(DIST, 'texlive/texmf-dist'), # ':'.join(os.path.join(texmf, 'texmf-dist') for texmf in texmf_system + texmf_local])
@@ -105,7 +113,6 @@ def pdflatex(tex_relative_path, busytex, cwd, DIST, bibtex, log = None):
         SOURCE_DATE_EPOCH = str(SOURCE_DATE_EPOCH) # https://wiki.debian.org/ReproducibleBuilds/TimestampsInPDFGeneratedByLaTeX
     )
 
-    has_error = lambda cmdres, errors: any(e.encode() in cmdres.stdout + cmdres.stderr for e in errors)
     collect_logs = lambda cmdres, errors, aux_path = '': dict( vars(cmdres), has_error = has_error(cmdres, errors), texlog = read_all_bytes(log_path), biblog = read_all_bytes(blg_path), texmflog = read_all_bytes(texmflog), missfontlog = read_all_bytes(missfontlog), aux = read_all_bytes(aux_path) )
 
     arg_pdftex_verbose = ['-kpathsea-debug', str(KPSE_DEBUG_SEARCH)] # https://www.tug.org/texinfohtml/kpathsea.html#Debugging You can get this by setting the environment variable KPATHSEA_DEBUG to ‘-1’
@@ -162,34 +169,87 @@ def pdflatex(tex_relative_path, busytex, cwd, DIST, bibtex, log = None):
     
     return logs
 
-def xelatex():
-    xetex     = ['xelatex' ,   '--no-shell-escape', '--interaction=batchmode', '--halt-on-error', '--no-pdf'           , '--fmt', this.fmt.xetex , tex_path]
-    xdvipdfmx = ['xdvipdfmx', '-o', pdf_path, xdv_path]
-    arg_xetex_debug = ['-kpathsea-debug', '63', '-recorder']
-    arg_xetex_verbose = ['-kpathsea-debug', '32']
-    arg_xdvipdfmx_verbose = ['--kpathsea-debug','32', '-v']
-    arg_xdvipdfmx_debug = ['--kpathsea-debug','63', '-vv']
-    FMT = os.path.join(DIST, 'xelatex.fmt')
-    #xdvipdfmx:warning: Could not locate a virtual/physical font for TFM "ec-qhvr".
-    #xdvipdfmx:warning: >> There are no valid font mapping entry for this font.
-    #xdvipdfmx:warning: >> Font file name "ec-qhvr" was assumed but failed to locate that font.
-    #xdvipdfmx:fatal: Cannot proceed without .vf or "physical" font for PDF output...
-    #No output PDF file written.
-    #xdvipdfmx:warning: Color stack underflow. Just ignore.
-    #xdvipdfmx:warning: Color stack underflow. Just ignore.
-    #xdvipdfmx:warning: Color stack underflow. Just ignore.
-    #xdvipdfmx:warning: Color stack underflow. Just ignore.
-    #xdvipdfmx:warning: Color stack underflow. Just ignore.
-    #xdvipdfmx:warning: Could not locate a virtual/physical font for TFM "ec-qhvr".
-    #xdvipdfmx:warning: >> There are no valid font mapping entry for this font.
-    #xdvipdfmx:warning: >> Font file name "ec-qhvr" was assumed but failed to locate that font.
-    #xdvipdfmx:fatal: Cannot proceed without .vf or "physical" font for PDF output...
-    #$BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XELATEXFMT example.tex
-    #$BUSYTEX bibtex8 --8bit example.aux
-    #$BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XELATEXFMT example.tex
-    #$BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --fmt $XELATEXFMT example.tex
-    #$BUSYTEX xdvipdfmx -o example_xelatex.pdf example.xdv
-    pass
+def xelatex(tex_relative_path, busytex, cwd, DIST, bibtex, log = None):
+    # http://tug.ctan.org/info/tex-font-errors-cheatsheet/tex-font-cheatsheet.pdf 
+    texmflog = 'texmf.log' # /tmp/texmf.log
+    missfontlog = 'missfont.log'
+    fmt = os.path.join(DIST, 'xelatex.fmt')
+    pdf_path, log_path, aux_path, blg_path, bbl_path, xdv_path  = [tex_relative_path.removesuffix('.tex') + ext for ext in ['.pdf', '.log', '.aux', '.blg', '.bbl', '.xdv', ]] # https://github.com/github/gitignore/blob/main/TeX.gitignore
+
+    env = dict(
+        TEXMFDIST         = os.path.join(DIST, 'texlive/texmf-dist'), # ':'.join(os.path.join(texmf, 'texmf-dist') for texmf in texmf_system + texmf_local])
+        TEXMFCNF          = os.path.join(DIST, 'texlive/texmf-dist/web2c'),
+        TEXMFVAR          = os.path.join(DIST, 'texlive/texmf-dist/texmf-var'), # TODO: change to a separate out-of-tree non-readonly directory
+        TEXMFLOG          = texmflog, 
+        FONTCONFIG_PATH   = DIST, # /etc/fonts
+        FC_DEBUG          = str(FONTCONFIG_DEBUG_MATCHV),# https://www.freedesktop.org/software/fontconfig/fontconfig-user.html
+        SOURCE_DATE_EPOCH = str(SOURCE_DATE_EPOCH) # https://wiki.debian.org/ReproducibleBuilds/TimestampsInPDFGeneratedByLaTeX
+    )
+
+    collect_logs = lambda cmdres, errors, aux_path = '': dict( vars(cmdres), has_error = has_error(cmdres, errors), texlog = read_all_bytes(log_path), biblog = read_all_bytes(blg_path), texmflog = read_all_bytes(texmflog), missfontlog = read_all_bytes(missfontlog), aux = read_all_bytes(aux_path) )
+
+    arg_xdvipdfmx_verbose = ['--kpathsea-debug', str(KPSE_DEBUG_SEARCH), '-v']
+    arg_xdvipdfmx_debug = ['--kpathsea-debug', str(KPSE_DEBUG_EVERYTHING), '-vv']
+    
+    arg_xetex_verbose = ['-kpathsea-debug', str(KPSE_DEBUG_SEARCH)] # https://www.tug.org/texinfohtml/kpathsea.html#Debugging You can get this by setting the environment variable KPATHSEA_DEBUG to ‘-1’
+    arg_xetex_debug = ['-kpathsea-debug', str(KPSE_DEBUG_EVERYTHING), '-recorder']
+    arg_bibtex_verbose = ['--debug', 'search']
+    arg_bibtex_debug = ['--debug', 'all']
+    cmd_bibtex = [busytex, 'bibtex8', '--8bit', tex_relative_path.removesuffix('.tex') + '.aux']
+    
+    cmd_xetex     = [busytex, 'xelatex' ,   '--no-shell-escape', '--interaction=batchmode', '--halt-on-error', '--no-pdf'           , '--fmt', this.fmt.xetex , tex_path] #TODO: nonstopmode?
+    cmd_xdvipdfmx = ['xdvipdfmx', '-o', pdf_path, xdv_path]
+
+    logs = []
+    
+    if bibtex:
+        cmd1res = subprocess.run(cmd_xetex, env = env, cwd = cwd, capture_output = True)
+        logs.append(collect_logs(cmd1res, error_messages_fatal, aux_path))
+        
+        cmd2res = subprocess.run(cmd_bibtex, env = env, cwd = cwd, capture_output = True)
+        logs.append(collect_logs(cmd2res, error_messages_fatal, bbl_path))
+        
+        cmd3res = subprocess.run(cmd_xetex, env = env, cwd = cwd, capture_output = True)
+        logs.append(collect_logs(cmd3res, error_messages_fatal, aux_path))
+        
+        cmd4res = subprocess.run(cmd_xetex, env = env, cwd = cwd, capture_output = True)
+        logs.append(collect_logs(cmd4res, error_messages_all, aux_path))
+
+        cmd5res = subprocess.run(cmd_xdvipdfmx, env = env, cwd = cwd, capture_output = True)
+        logs.append(collect_logs(cmd5res, error_messages_all, aux_path))
+    else:
+        cmd4res = subprocess.run(cmd_pdftex, env = env, cwd = cwd, capture_output = True)
+        logs.append(collect_logs(cmd4res, error_messages_all, aux_path))
+
+        cmd5res = subprocess.run(cmd_xdvipdfmx, env = env, cwd = cwd, capture_output = True)
+        logs.append(collect_logs(cmd5res, error_messages_all, aux_path))
+
+    if log:
+        logcat = b'\n\n'.join(b'\n'.join([
+                b'$ ' + ' '.join(log['args']).encode(), 
+                b'EXITCODE: ' + str(log['returncode']).encode(), 
+                b'', 
+                b'TEXMFLOG:', 
+                log.get('texmflog', b''), 
+                b'==', 
+                b'MISSFONTLOG:', 
+                log.get('missfontlog', b''), 
+                b'==', 
+                b'LOG:', 
+                log.get('log', b''), 
+                b'==', 
+                b'STDOUT:', 
+                log['stdout'], 
+                b'==', 
+                b'STDERR:', 
+                log['stderr'], 
+                b'======'
+            ]) for log in logs)
+
+        with open(log, 'wb') as f:
+            f.write(logcat)
+    
+    return logs
 
 def lualatex():
     luahbtex  = ['luahblatex', '--no-shell-escape', '--interaction=nonstopmode', '--halt-on-error', '--output-format=pdf', '--fmt', FMT, '--nosocket', tex_path]
@@ -204,16 +264,13 @@ def prepare_tex_params(dirname):
     #const basename = this.PATH.basename(dirname);
     #const tex_files = this.find(dirname, '', false).filter(f => f.contents != null && f.path.endsWith(this.tex_ext));
     #let default_path = null;
-    #
     #if(tex_files.length == 1)
     #    default_path = tex_files[0].path;
-
     #else if(tex_files.length > 1)
     #{
     #    const main_tex_files = this.find(dirname, '', false).filter(f => f.contents != null && f.path.endsWith(this.tex_ext) && (f.path.includes('main') || f.path.includes(basename)));
     #    default_path = main_tex_files.length > 0 ? main_tex_files[0].path : tex_files[0].path;
     #}
-
     #if(default_path == null)
     #{
     #    const text_files = this.find(dirname, '', false).filter(f => f.contents != null && this.text_extensions.some(ext => f.path.toLowerCase().endsWith(ext)));
@@ -225,10 +282,9 @@ def prepare_tex_params(dirname):
     #        default_path = main_text_files.length > 0 ? main_text_files[0].path : text_files[0].path;
     #    }
     #}
-#    if bibtex is None:
-#        bibtex = any(bib_cmd in open(path).read() for path in file_paths if path.endswith('.tex') for bib_cmd in ['\\bibliography', '\\printbibliography'])
-#        // files.some(({path, contents}) => contents != null && path.endsWith('.bib'));
-#        bibtex = 
+    #if bibtex is None:
+    #    bibtex = any(bib_cmd in open(path).read() for path in file_paths if path.endswith('.tex') for bib_cmd in ['\\bibliography', '\\printbibliography'])
+    #    // files.some(({path, contents}) => contents != null && path.endsWith('.bib'));
    
     prevcwd = os.getcwd()
     os.chdir(dirname)
@@ -262,7 +318,14 @@ def main(args):
     if args.driver == 'pdflatex':
         logs = pdflatex(**tex_params, busytex = os.path.abspath(args.busytex), DIST = os.path.abspath(args.DIST), log = args.log)
         output_exists = os.path.exists(os.path.join(tex_params['cwd'], tex_params['tex_relative_path'].removesuffix('.tex') + '.pdf'))
-
+        if logs[-1]['returncode'] == 0 and not logs[-1]['has_error']:
+            return print(args.input_dir, 'OK', tex_params, output_exists, args.log)
+        else:
+            return print(args.input_dir, 'FAIL', tex_params, output_exists, args.log)
+    
+    if args.driver == 'xelatex':
+        logs = xelatex(**tex_params, busytex = os.path.abspath(args.busytex), DIST = os.path.abspath(args.DIST), log = args.log)
+        output_exists = os.path.exists(os.path.join(tex_params['cwd'], tex_params['tex_relative_path'].removesuffix('.tex') + '.pdf'))
         if logs[-1]['returncode'] == 0 and not logs[-1]['has_error']:
             return print(args.input_dir, 'OK', tex_params, output_exists, args.log)
         else:
