db07cd0ec4c163ac0444c938b7b5ba5a48505755
Vadim Kantorov <vadimkantorov@gmail.com>
2024-03-07 00:05:34 +0100

    ...

--- busytexmk.py
+++ busytexmk.py
@@ -294,9 +294,9 @@ def main(args):
     tar = tarfile.open(args.tar) if args.tar else None
     if tar is not None:
         members = [member for member in tar.getmembers() if os.path.basename(member.name) == os.path.basename(args.input_dir)]
-        data = gzip.open(fileobj = tar.extractfile(members[0].path)).read()
+        data = gzip.open(tar.extractfile(members[0].path)).read()
         try:
-            tarfile.open(io.BytesIO(data)).extractall('.')
+            tarfile.open(fileobj = io.BytesIO(data)).extractall('.')
             print('extracted', args.input_dir)
         except Exception as e:
             print('failed', args.input_dir)
