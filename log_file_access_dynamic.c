70dc112226a6fd47f537a348f5f6c69e14b452f4
Vadim Kantorov <vadimkantorov@gmail.com>
2024-09-05 16:14:33 +0200

    ...

--- log_file_access_dynamic.c
+++ log_file_access_dynamic.c
@@ -24,14 +24,14 @@ int open(const char *path, int flags, ...)
     orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "open");
     return orig_func(path, flags);
 }
-int open64(const char *path, int flags, ...)
+
+int open64(const char *path, int flags, mode_t mode)
 {
-    typedef int (*orig_func_type)(const char *pathname, int flags);
+    typedef int (*orig_func_type)(const char *pathname, int flags, mode_t mode);
     fprintf(stderr, "log_file_access_preload: open64(\"%s\", %d)\n", path, flags);
     orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "open64");
-    return orig_func(path, flags);
+    return orig_func(path, flags, mode);
 }
-
 //int openat(int dirfd, const char *path, int flags, ...)
 //{
 //    typedef int (*orig_func_type)(int dirfd, const char *pathname, int flags);
