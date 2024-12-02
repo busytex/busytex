2abad72ad8b49b89dde73ca0bd5d2524efaa0a98
Vadim Kantorov <vadimkantorov@gmail.com>
2024-12-02 18:11:31 +0100

    ...

--- busypack.py
+++ busypack.py
@@ -1,56 +1,47 @@
 import os
+import re
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser()
-my $input_path = '';
-my $output_path = '';
-my $prefix = '';
-my $ld = 'ld';
-my $skip = '';
-Getopt::Long::GetOptions(
-    'input-path|i=s'  => \$input_path,
-    'output-path|o=s' => \$output_path,
-    'prefix=s'        => \$prefix,
-    'ld=s'            => \$ld,
-    'skip=s'          => \$skip
-);
+parser.add_argument('--input-path', '-i')
+parser.add_argument('--output-path', '-o')
+parser.add_argument('--prefix')
+parser.add_argument('--ld', default = 'ld')
+parser.add_argument('--skip', default = '')
 args = parser.parse_args()
 
-die "Input path does not exist or is not a directory" unless -e $input_path && -d $input_path ;
-die "Output path not specified" if $output_path eq '';
+assert args.input_path and os.path.exists(args.input_path) and os.path.isdir(args.input_path), "Input path does not exist or is not a directory"
+assert args.output_path, "Output path not specified"
 
-File::Path::make_path($output_path . '.o');
+os.makedirs(args.output_path + '.o', exist_ok = True)
 
-my $oldcwd = Cwd::getcwd();
-my (@objects, @files, @dirs_relpaths, @safepaths, @relpaths);
-File::Find::find(sub {
-    my $newcwd = Cwd::getcwd(); chdir $oldcwd; 
-    my $p = $File::Find::name;
-    my $safepath = $p; $safepath =~ s/[\/.-]/_/g;
-    my $relpath = (split(/\//, $p, 2))[-1];
+objects, files, dirs_relpaths, safepaths, relpaths = [], [], [], [], []
 
-    if (-d $p) {
-        push @dirs_relpaths, $p;
-    } elsif ($skip eq '' or $p !~ /$skip\z/) {
-        push @files, $p;
-        push @safepaths, $safepath;
-        push @relpaths, $relpath;
-        push @objects, File::Spec->catfile($output_path . '.o', $safepath . '.o');
-        system($ld, '-r', '-b', 'binary', '-o', $objects[-1], $files[-1]) == 0 or die "ld command failed: $?";
-    }
-    chdir $newcwd;
-}, $input_path);
+for (dirpath, dirnames, filenames) in os.walk(args.input_path):
+    dirs_relpaths.extend(os.path.join(dirpath, basename).split(os.path.sep, maxsplit = 1)[-1] for basename in dirnames)
+    
+    for basename in filenames:
+        p = os.path.join(dirpath, basename)
+        safepath = p.translate({ord('.') : '_', ord('-') : '_'})
+        
+        relpath = p.split(os.path.sep, maxsplit = 1)[-1]
+        if not args.skip or not re.match('.+(' + args.skip + ')$', basename):
+            files.append(p)
+            safepaths.append(safepath)
+            relpaths.append(relpath)
+            objects.append(os.path.join(args.output_path + '.o', safepath + '.o'))
+            subprocess.check_call([args.ld, '-r', '-b', 'binary', '-o', objects[-1], files[-1])
 
 # problem: can produce the same symbol name because of this mapping
 
-open my $g, '>', $output_path . '.txt' or die;
-print $g join("\n", @objects);
+g = open(args.output_path + '.txt', 'w')
+print('\n'.join(objects), file = g)
 
-open my $f, '>', $output_path or die;
-print $f "size_t packfs_builtin_files_num = ", scalar(@files), ";\n\n";
-print $f join("\n", map { "extern char _binary_${_}_start[], _binary_${_}_end[];" } @safepaths), "\n\n";
-
-print $f "const char* packfs_builtin_safepaths[] = {\n\"", join("\",\n\"", @safepaths), "\"\n};\n";
-print $f "const char* packfs_builtin_abspaths[] = {\n\"" , join("\",\n\"", map { File::Spec->catfile($prefix, $_) } @relpaths), "\"\n};\n\n";
-print $f "const char* packfs_builtin_starts[] = {\n", join("\n", map { "_binary_${_}_start," } @safepaths), "\n};\n\n";
-print $f "const char* packfs_builtin_ends[] = {\n", join("\n", map { "_binary_${_}_end," } @safepaths), "\n};\n\n";
+f = open(args.output_path, 'w')
+print("size_t packfs_builtin_files_num = ", len(files), ";\n\n", file = f)
+print("\n".join(f"extern char _binary_{_}_start[], _binary_{_}_end[];" for _ in safepaths), "\n\n", file = f)
+print("const char* packfs_builtin_safepaths[] = {\n\"", "\",\n\"".join(safepaths), "\"\n};\n", file = f)
+print("const char* packfs_builtin_abspaths[] = {\n\"" , "\",\n\"".join(os.path.join(args.prefix, _) for _ in relpaths), "\"\n};\n\n", file = f)
+print("const char* packfs_builtin_starts[] = {\n", "\n".join(f"_binary_{_}_start," for _ in safepaths), "\n};\n\n", file = f)
+print("const char* packfs_builtin_ends[] = {\n", "\n".join(f"_binary_{_}_end," for _ in safepaths), "\n};\n\n";
