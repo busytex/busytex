4aca3ae5418da12d5233d6111977ef0e66405784
Vadim Kantorov <vadimkantorov@gmail.com>
2024-12-09 17:38:42 +0100

    ...

--- busypack.c
+++ busypack.c
@@ -44,6 +44,8 @@ struct packfs_context
     const char** packfs_builtin_abspaths_dirs;
 };
 
+extern int orig_stat(const char *restrict path, struct stat *restrict statbuf); 
+
 struct packfs_context* packfs_ensure_context()
 {
     static struct packfs_context packfs_ctx = {0};
@@ -55,7 +57,8 @@ struct packfs_context* packfs_ensure_context()
         extern ssize_t orig_read(int fd, void* buf, size_t count); packfs_ctx.orig_read = orig_read;
         extern int orig_access(const char *path, int flags); packfs_ctx.orig_access = orig_access;
         extern off_t orig_lseek(int fd, off_t offset, int whence); packfs_ctx.orig_lseek = orig_lseek;
-        extern int orig_stat(const char *restrict path, struct stat *restrict statbuf); packfs_ctx.orig_stat = orig_stat;
+        packfs_ctx.orig_stat = orig_stat;
+        fprintf(stderr, "orig_stat: %p\n", (void*)orig_stat);
         extern int orig_fstat(int fd, struct stat * statbuf); packfs_ctx.orig_fstat = orig_fstat;
         extern FILE* orig_fopen(const char *path, const char *mode); packfs_ctx.orig_fopen = orig_fopen;
         extern int orig_fileno(FILE* stream); packfs_ctx.orig_fileno = orig_fileno;
