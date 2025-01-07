3dae84c8fbb70d16fe25284e008a6ffe62405721
Vadim Kantorov <vadimkantorov@gmail.com>
2025-01-07 21:57:57 +0200

    ...

--- example/dist.sh
+++ example/dist.sh
@@ -26,7 +26,7 @@ echo "collection-xetex  1"                          >> $DIST.profile
 echo "collection-latex  1"                          >> $DIST.profile  
 echo "collection-luatex 1"                          >> $DIST.profile  
 
-TEXLIVE_INSTALL_NO_RESUME=1 perl installer/install-tl --repository source/texmfrepo --profile $DIST.profile --custom-bin $TEXDIR/$BINARCH_native --no-doc-install --no-src-install
+TEXLIVE_INSTALL_NO_RESUME=1 perl installer/install-tl --profile $DIST.profile --custom-bin $TEXDIR/$BINARCH_native --no-doc-install --no-src-install
 echo '<?xml version="1.0"?><!DOCTYPE fontconfig SYSTEM "fonts.dtd"><fontconfig><dir>/texlive/texmf-dist/fonts/opentype</dir><dir>/texlive/texmf-dist/fonts/type1</dir></fontconfig>' > $DIST/fonts.conf
 
 #mv $(basename $@)/texmf-dist/texmf-var/web2c/luahbtex/lualatex.fmt $(basename $@)/texmf-dist/texmf-var/web2c/luahbtex/luahblatex.fmt
