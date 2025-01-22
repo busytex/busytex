35f5c04b81721e884869fe1f34de6cdfaaa25645
Vadim Kantorov <vadimkantorov@gmail.com>
2025-01-22 14:59:52 +0200

    ...

--- example/dist.sh
+++ example/dist.sh
@@ -9,7 +9,11 @@ BUSYTEX_native=busytex
 BINARCH_native=bin/_custom
 TEXDIR=$PWD/$DIST
 
-mkdir -p $DIST/$BINARCH_native && curl -o $BUSYTEX_native -L https://github.com/busytex/busytex/releases/download/build_native_9b40c3ce65d39b52bc38eb4794b8f9837b956064_12299351715_1/busytex && chmod +x $BUSYTEX_native && ln -s $PWD/busytex $TEXDIR/$BINARCH_native/$BUSYTEX_native
+if [ ! -f $BUSYTEX_native ]; then
+    curl -o $BUSYTEX_native -L https://github.com/busytex/busytex/releases/download/build_native_9b40c3ce65d39b52bc38eb4794b8f9837b956064_12299351715_1/busytex && chmod +x $BUSYTEX_native
+fi
+
+mkdir $DIST/$BINARCH_native && ln -s $PWD/busytex $TEXDIR/$BINARCH_native/$BUSYTEX_native
 
 # https://github.com/busytex/busytex/releases/download/texlive2023-20230313.iso/install-tl-unx.tar.gz
 mkdir -p $DIST/installer && curl -L https://github.com/busytex/busytex/releases/download/texlive2023-20230313.iso/install-tl-unx.tar.gz    | tar -xzf - --strip-components=1             -C $DIST/installer 
