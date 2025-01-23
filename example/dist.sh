9ebbaeb216babdac0c621b42e1ff691f85832df1
Vadim Kantorov <vadimkantorov@gmail.com>
2025-01-23 15:33:02 +0200

    ...

--- example/dist.sh
+++ example/dist.sh
@@ -7,12 +7,8 @@ BUSYTEX_native=busytex
 BINARCH_native=bin/_custom
 TEXDIR=$PWD/$DIST
 
-#[ ! -e $BUSYTEX_native ] && curl -o $BUSYTEX_native -L https://github.com/busytex/busytex/releases/download/build_native_9b40c3ce65d39b52bc38eb4794b8f9837b956064_12299351715_1/busytex && chmod +x $BUSYTEX_native
-#mkdir -p $DIST/$BINARCH_native && ln -s $PWD/busytex $TEXDIR/$BINARCH_native/$BUSYTEX_native
-
 mkdir -p $DIST/$BINARCH_native && curl -o $DIST/$BINARCH_native/$BUSYTEX_native -L https://github.com/busytex/busytex/releases/download/build_native_9b40c3ce65d39b52bc38eb4794b8f9837b956064_12299351715_1/busytex && chmod +x $DIST/$BINARCH_native/$BUSYTEX_native
 ln -s $TEXDIR/$BINARCH_native/$BUSYTEX_native $BUSYTEX_native
-#mkdir -p $DIST/$BINARCH_native && ln -s $PWD/busytex $TEXDIR/$BINARCH_native/$BUSYTEX_native
 
 # https://github.com/busytex/busytex/releases/download/texlive2023-20230313.iso/install-tl-unx.tar.gz
 mkdir -p $DIST/installer && curl -L https://github.com/busytex/busytex/releases/download/texlive2023-20230313.iso/install-tl-unx.tar.gz    | tar -xzf - --strip-components=1             -C $DIST/installer 
@@ -26,7 +22,7 @@ curl --output-dir $DIST -O -L https://github.com/busytex/busytex/releases/downlo
 for name in xetex luahbtex pdftex xelatex luahblatex pdflatex kpsewhich kpseaccess kpsestat kpsereadlink; do
     printf "#!/bin/sh\n$TEXDIR/$BINARCH_native/busytex $name \$@" > $DIST/$BINARCH_native/$name && chmod +x $DIST/$BINARCH_native/$name
 done
-for name in updmap.pl fmtutil.pl mktexlsr.pl; do  #updmap-sys.sh updmap-user.sh fmtutil-sys.sh fmtutil-user.sh; do
+for name in updmap.pl fmtutil.pl mktexlsr.pl updmap-sys.sh updmap-user.sh fmtutil-sys.sh fmtutil-user.sh; do
     cp $DIST/texmf-dist/scripts/texlive/$name $DIST/$BINARCH_native/${name%.*}
 done
 
@@ -42,4 +38,3 @@ echo "collection-luatex 1"                          >> $DIST/$DIST.profile
 TEXLIVE_INSTALL_NO_RESUME=1 perl $DIST/installer/install-tl --profile $DIST/$DIST.profile --custom-bin $TEXDIR/$BINARCH_native --no-doc-install --no-src-install --no-interaction
 echo '<?xml version="1.0"?><!DOCTYPE fontconfig SYSTEM "fonts.dtd"><fontconfig><dir>/texlive/texmf-dist/fonts/opentype</dir><dir>/texlive/texmf-dist/fonts/type1</dir></fontconfig>' > $DIST/fonts.conf
 #mv $(basename $@)/texmf-dist/texmf-var/web2c/luahbtex/lualatex.fmt $(basename $@)/texmf-dist/texmf-var/web2c/luahbtex/luahblatex.fmt
-echo $(realpath $(dirname $BUSYTEX_native))
