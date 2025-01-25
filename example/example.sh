93b4f35d813bb62a7f25d4234fba937e97a39c45
Vadim Kantorov <vadimkantorov@gmail.com>
2025-01-25 11:43:18 +0100

    ...

--- example/example.sh
+++ example/example.sh
@@ -14,8 +14,6 @@ if [[ "$1" == "busytex" ]]; then
     export FONTCONFIG_PATH=$DIST/texlive-dist
 fi
 
-echo EXAMPLE: 1:"$1" DIST:"$DIST" TEXMFDIST:"$TEXMFDIST"
-
 if [ -d example ]; then
     cd example
 fi
@@ -27,18 +25,18 @@ if [[ "$ENGINES" == *"pdflatex"* ]]; then
     $BUSYTEX pdflatex --no-shell-escape --interaction nonstopmode --draftmode --halt-on-error --output-format=pdf --progname pdflatex example.tex
     $BUSYTEX pdflatex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --progname pdflatex --jobname example_pdflatex.pdf example.tex
 fi
-#if [[ "$ENGINES" == *"xelatex"* ]]; then
-#    # --fmt $TEXMFVAR/web2c/xetex/xelatex.fmt
-#    $BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --progname xelatex  example.tex
-#    $BUSYTEX bibtex8 --8bit example.aux
-#    $BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --progname xelatex example.tex
-#    $BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --progname xelatex example.tex
-#    $BUSYTEX xdvipdfmx -o example_xelatex.pdf example.xdv
-#fi
-#if [[ "$ENGINES" == *"luahblatex"* ]]; then
-#    # --fmt $TEXMVAR/web2c/luahbtex/luahblatex.fmt
-#    $BUSYTEX luahblatex --no-shell-escape --interaction nonstopmode --draftmode --halt-on-error --output-format=pdf --progname luahblatex  --nosocket example.tex
-#    $BUSYTEX bibtex8 --8bit example.aux
-#    $BUSYTEX luahblatex --no-shell-escape --interaction nonstopmode --draftmode --halt-on-error --output-format=pdf --progname luahblatex --nosocket example.tex
-#    $BUSYTEX luahblatex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --progname luahblatex --jobname example_luahblatex.pdf --nosocket example.tex
-#fi
+if [[ "$ENGINES" == *"xelatex"* ]]; then
+    # --fmt $TEXMFVAR/web2c/xetex/xelatex.fmt
+    $BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --progname xelatex  example.tex
+    $BUSYTEX bibtex8 --8bit example.aux
+    $BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --progname xelatex example.tex
+    $BUSYTEX xelatex --no-shell-escape --interaction nonstopmode --halt-on-error --no-pdf --progname xelatex example.tex
+    $BUSYTEX xdvipdfmx -o example_xelatex.pdf example.xdv
+fi
+if [[ "$ENGINES" == *"luahblatex"* ]]; then
+    # --fmt $TEXMVAR/web2c/luahbtex/luahblatex.fmt
+    $BUSYTEX luahblatex --no-shell-escape --interaction nonstopmode --draftmode --halt-on-error --output-format=pdf --progname luahblatex  --nosocket example.tex
+    $BUSYTEX bibtex8 --8bit example.aux
+    $BUSYTEX luahblatex --no-shell-escape --interaction nonstopmode --draftmode --halt-on-error --output-format=pdf --progname luahblatex --nosocket example.tex
+    $BUSYTEX luahblatex --no-shell-escape --interaction nonstopmode --halt-on-error --output-format=pdf --progname luahblatex --jobname example_luahblatex.pdf --nosocket example.tex
+fi
