64de1066a9e3af3bb3cfa6924746ca497d4ef457
Claudio Bartolini <claudioluisbartolini@gmail.com>
2025-02-26 16:08:15 -0800

    cleaned up main.js imports

--- public/main.js
+++ public/main.js
@@ -1,12 +1,7 @@
-// Ensure this file is imported as type="module" in index.html
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
-import { db } from './firebase-config.js';
 import { loadProjectsFromFirestore, explorerTree } from './projectManager.js';
 import { initializeEditor } from './editorManager.js';
 import { renderFileExplorer, setupContextMenuHandlers } from './uiManager.js';
-import { onclick_, terminate } from './compileManager.js';
 
 // Load projects and initialize UI
 async function initApp() {
